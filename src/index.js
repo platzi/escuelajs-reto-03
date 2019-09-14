16b8d3934b476448f1113407fa275b7025bca1ba
AryRosvall <ary.rosvall@gmail.com>
2019-09-14 13:31:39 -0500

    Arantxa Rosas, Ciudad de México, reto 1, 2, 3

--- src/index.js
+++ src/index.js
@@ -1,32 +1,55 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+/* 
+.
+Sept 14, 2019
+escuelajs-reto-03
+Arantxa Rosas
+Ciudad de México 
+Reto 1,2 y 3
+.
+*/ 
 
-var API = 'https://rickandmortyapi.com/api/character/';
-var xhttp = new XMLHttpRequest();
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
-      if (xhttp.status == 200)
-        callback(null, xhttp.responseText);
-      else return callback(url_api);
-    }
-  };
-  xhttp.open('GET', url_api, false);
-  xhttp.send();
+const url_api = "https://rickandmortyapi.com/api/character/";
+
+const fetchData = ({ method = "GET", url_api, async = false }) => {
+  
+  const xhttp = new XMLHttpRequest();
+  
+  return new Promise((resolve, reject) => {
+    xhttp.onreadystatechange = event => {
+      xhttp.readyState === 4 && xhttp.status === 200
+        ? resolve(JSON.parse(xhttp.responseText))
+        : reject();
+    };
+
+    //method, URL, is async?
+    xhttp.open(method, url_api, async);
+    xhttp.send();
+  });
 };
 
-fetchData(API, function (error1, data1) {
-  if (error1) return console.error('Error' + ' ' + error1);
-  console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
-    if (error2) return console.error(error1);
-    console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
-      if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
+const fetchingData = async () => {
+  try {
+    let resultado1 = await fetchData({ url_api });
+
+    console.log(`Primer Llamado...`);
+
+    let resultado2 = await fetchData({
+      url_api: `${url_api}\\${resultado1.results[0].id}`
     });
-  });
-});
+
+    console.log(`Segundo Llamado...`);
+
+    let resultado3 = await fetchData({ url_api: resultado2.origin.url });
+
+    console.log(
+      `Tercer Llamado...:\n ${resultado1.info.count}\n Primer Personaje: ${resultado2.name} \nDimensión: ${resultado3.dimension}`
+    );
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+fetchingData();
