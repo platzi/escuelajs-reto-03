1fc9f77c939448097103764b6c4d0d06bbc4862f
David R <deveeup@MacBook-Pro.local>
2019-09-14 12:00:45 -0500

    need pull request

--- src/index.js
+++ src/index.js
@@ -1,32 +1,39 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var API = 'https://rickandmortyapi.com/api/character/';
-var xhttp = new XMLHttpRequest();
+const BASE_API = "https://rickandmortyapi.com/api/character/",
+  xhr = new XMLHttpRequest();
 
-function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
-      if (xhttp.status == 200)
-        callback(null, xhttp.responseText);
-      else return callback(url_api);
+const fetchData = (url_api, callback) => {
+  xhr.open("GET", url_api, false);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        return callback(null, xhr.responseText);
+      } else {
+        console.log("error!");
+      }
     }
   };
-  xhttp.open('GET', url_api, false);
-  xhttp.send();
+  xhr.send();
 };
 
-fetchData(API, function (error1, data1) {
-  if (error1) return console.error('Error' + ' ' + error1);
-  console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
-    if (error2) return console.error(error1);
-    console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
-      if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
+fetchData(BASE_API, (error_one, data_one) => {
+  if (error_one) return console.error(`error: ${error_one}`);
+  const convertJSON = JSON.parse(data_one),
+    idPerson = convertJSON.results[0].id,
+    API_PERSON = `${BASE_API}${idPerson}`;
+
+  fetchData(API_PERSON, (error_two, data_two) => {
+    if (error_two) return console.error(`error: ${error_two}`);
+    const convertJSON2 = JSON.parse(data_two),
+      URL_PERSON = convertJSON2.origin.url;
+
+    fetchData(URL_PERSON, (error_three, data_three) => {
+      if (error_three) return console.error(`error: ${error_three}`);
+      const person = JSON.parse(data_three);
+      console.log(`Person: ${person.name}`);
+      console.log(`Dimension: ${person.dimension}`);
+      console.log(`Planet: ${person.type}`);
     });
   });
 });
