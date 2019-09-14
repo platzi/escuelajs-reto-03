15271418a9c884f9f2731925bbc857bce476eede
Gerardo Marquez Carmona <gerardomarquezcarmona@MacBook-Air-de-Gerardo.local>
2019-09-14 13:03:59 -0500

    Solucion  Reto 3

--- src/index.js
+++ src/index.js
@@ -1,32 +1,24 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const fetch = require("node-fetch");
 
-var API = 'https://rickandmortyapi.com/api/character/';
-var xhttp = new XMLHttpRequest();
+const url_api = "https://rickandmortyapi.com/api/character/";
+async function getFetch(url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
+}
 
-function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
-      if (xhttp.status == 200)
-        callback(null, xhttp.responseText);
-      else return callback(url_api);
-    }
-  };
-  xhttp.open('GET', url_api, false);
-  xhttp.send();
-};
-
-fetchData(API, function (error1, data1) {
-  if (error1) return console.error('Error' + ' ' + error1);
-  console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
-    if (error2) return console.error(error1);
-    console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
-      if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
-    });
-  });
-});
+(async function() {
+  try {
+    console.log(`Primer Llamado...`);
+    let data1 = await getFetch(url_api);
+    console.log(`Segundo Llamado...`);
+    let data2 = await getFetch(`${url_api}${data1.results[0].id}`);
+    let data3 = await getFetch(`${data2.origin.url}`);
+    console.log(`Tercero Llamado...`);
+    console.log(`Personajes: ${data1.info.count}`);
+    console.log(`Primer Personaje: ${data2.name}`);
+    console.log(`Dimensión: ${data3.dimension}`);
+  } catch {
+    console.log(`Error al obtener archivos`);
+  }
+})();
