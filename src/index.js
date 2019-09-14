50929176be47b9000c0065717d9c085fa503e3b3
Edwin García <spark.com.co@gmail.com>
2019-09-14 18:49:04 -0500

    tercera parte

--- src/index.js
+++ src/index.js
@@ -1,32 +1,37 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
 
-var API = 'https://rickandmortyapi.com/api/character/';
-var xhttp = new XMLHttpRequest();
+const API = 'https://rickandmortyapi.com/api/character/'
+const xhttp = new XMLHttpRequest()
 
-const fetchData = (url_api, callback) => {
-  xhttp.onreadystatechange = event => {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status == 200){
-        callback(null, JSON.parse(xhttp.responseText));
-      }else return callback(url_api);
+const fetchData = (url_api) => {
+  return new Promise((resolve, reject) => {
+    xhttp.onreadystatechange = event => {
+      if (xhttp.readyState === 4) {
+        if (xhttp.status == 200){
+          resolve(JSON.parse(xhttp.responseText))
+        }else reject(url_api)
+      }
     }
-  }
-  xhttp.open('GET', url_api, false);
-  xhttp.send();
-};
+    xhttp.open('GET', url_api, false)
+    xhttp.send()
+  })
+}
 
-fetchData(API, (error1, data1) => {
-  if (error1) return console.error(`Error ${error1}`);
-  console.log('Primer Llamado...')
-  fetchData(`${API}${data1.results[0].id}`, (error2, data2) => {
-    if (error2) return console.error(error1);
+(async() => {
+  try {
+    const data1 = await fetchData(API)
+    console.log('Primer Llamado...')
+  
+    const data2 = await fetchData(`${API}${data1.results[0].id}`)
     console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, (error3, data3) => {
-      if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log(`Personajes: ${data1.info.count}`)
-      console.log(`Primer personaje: ${data2.name}`);
-      console.log(`Dimensión: ${data3.dimension}`);
-    });
-  });
-});
+  
+    const data3 = await fetchData(data2.origin.url)
+    console.log('Tercero Llamado...')
+  
+    console.log(`Personajes: ${data1.info.count}`)
+    console.log(`Primer personaje: ${data2.name}`)
+    console.log(`Dimensión: ${data3.dimension}`)
+  }catch(e){
+    console.error(`Error ${e}`)
+  }
+})()
