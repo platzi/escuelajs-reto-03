c8471121e863ab32b13281c2c9394614e06e7f4a
Diego Cardenas <diego.cardenas@ingenieros.com>
2019-09-14 12:23:51 -0500

    Problema 3 - Promesas

--- src/index.js
+++ src/index.js
@@ -2,29 +2,37 @@ const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 const API = 'https://rickandmortyapi.com/api/character/';
 const xhttp = new XMLHttpRequest();
-const fetchData = (url_api, callback) => {
-  xhttp.onreadystatechange = () => {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status == 200) callback(xhttp.responseText);
-      else return callback(url_api);
-    }
-  };
-  xhttp.open('GET', url_api);
-  xhttp.send();
+
+const fetchData = url_api => {
+  return new Promise((resolve, reject) => {
+    xhttp.onreadystatechange = () => {
+      if (xhttp.readyState === 4) {
+        if (xhttp.status === 200) {
+          resolve(JSON.parse(xhttp.responseText));
+        } else reject(url_api);
+      }
+    };
+    xhttp.open('GET', url_api);
+    xhttp.send();
+  });
 };
 
-fetchData(API, (error1, data1) => {
-  if (error1) return console.error(`Error ${error1}`);
-  console.log('Primer Llamado...');
-  fetchData(API + data1.results[0].id, (error2, data2) => {
-    if (error2) return console.error(error1);
-    console.log('Segundo Llamado...');
-    fetchData(data2.origin.url, (error3, data3) => {
-      if (error3) return console.error(error3);
-      console.log('Tercero Llamado...');
-      console.log(`Personajes ${data1.info.count}`);
-      console.log(`Primer Personaje ${data2.name}`);
-      console.log(`Dimension: ${data3.dimension}`);
-    });
-  });
-});
+const renderData = async () => {
+  try {
+    console.log(`Primer llamado`);
+    let data1 = await fetchData(API);
+    console.log(data1.results[0].id);
+    console.log('Segundo llamado...');
+    let data2 = await fetchData(API);
+    console.log(data2.results[0].url);
+    console.log('Tercer llamado');
+    let data3 = await fetchData(API);
+    console.log(`Personajes: ${data1.info.count}`);
+    console.log(`Primer Personaje: ${data2.results[0].name}`);
+    console.log(`Dimensión: ${data3.results[0].location.name}`);
+  } catch (error) {
+    console.error(`Error al consultar los datos`, error);
+  }
+};
+
+renderData();
