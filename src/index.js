f6e4c3765d74ee1c0ff8c88538648991704e7112
Jefferson Aguilar <jeffersonaguilar@outlook.com>
2019-09-14 11:21:47 -0500

    Optimizacion de codigo

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ const fetchData = (url_api) => {
   return new Promise((resolve, reject) => {
     xhttp.onreadystatechange = (event) => {
       if (xhttp.readyState === 4) {
-        if (xhttp.status === 200)
-          resolve(xhttp.responseText);
+        if (xhttp.status === 200) {
+          resolve(JSON.parse(xhttp.responseText));
+        }
         else
           reject(url_api);
       }
@@ -27,13 +28,10 @@ const obtenerPersonaje = (id) => {
 const llamadoDatos = async () => {
   console.log('Primer Llamado...');
   let data1 = await fetchData(API);
-  data1 = JSON.parse(data1);
   console.log('Segundo Llamado...');
   let data2 = await obtenerPersonaje(data1.results[0].id);
-  data2 = JSON.parse(data2);
-  let data3 = await fetchData(data2.origin.url);
-  data3 = JSON.parse(data3);
   console.log('Tercero Llamado...')
+  let data3 = await fetchData(data2.origin.url);
   console.log(`Personajes: ${data1.info.count}`);
   console.log(`Primer Personaje: ${data2.name}`);
   console.log(`Dimensión: ${data3.dimension}`);
