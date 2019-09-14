c27a67ecf2a36ddea4ab3000f5245d8c8b7c9e7c
wilantury <ventas@antury.net>
2019-09-14 11:13:06 -0500

    Solucion reto3- parte 1 y 2

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var API = 'https://rickandmortyapi.com/api/character/';
 var xhttp = new XMLHttpRequest();
 
-function fetchData(url_api, callback) {
+const fetchData = (url_api, callback) => {
   xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
+    if (xhttp.readyState === 4) {
       if (xhttp.status == 200)
         callback(null, xhttp.responseText);
       else return callback(url_api);
@@ -15,18 +15,21 @@ function fetchData(url_api, callback) {
   xhttp.send();
 };
 
-fetchData(API, function (error1, data1) {
-  if (error1) return console.error('Error' + ' ' + error1);
+fetchData(API, (error1, data1) => {
+  if (error1) return console.error(`Error ${error1}`);
   console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
+  const dataJson = JSON.parse(data1)
+  fetchData(API + dataJson.results[0].id, (error2, data2)=> {
     if (error2) return console.error(error1);
     console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
+    const data2Json = JSON.parse(data2)
+    fetchData(data2Json.origin.url, (error3, data3)=> {
+      const data3Json = JSON.parse(data3)
       if (error3) return console.error(error3);
       console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
+      console.log(`Personajes: ${dataJson.info.count}`)
+      console.log(`Primer Personaje: ${data2Json.name}`)
+      console.log(`Dimensión: ${data3Json.dimension}`);
     });
   });
 });
