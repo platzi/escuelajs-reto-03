efe61eacbadf87e18ae4305467b93c0653c9b1da
Julian Toro <jtoro@tecmovin.com>
2019-09-14 10:33:39 -0500

    organizando que funcione

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 var API = 'https://rickandmortyapi.com/api/character/';
-var xhttp = new XMLHttpRequest();
+
 
 function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
+  let xhttp = new XMLHttpRequest();
+  console.log("entro");
+  xhttp.onreadystatechange =  (event) => {
+    console.log("hola")
+    if (xhttp.readyState === 4) {
       if (xhttp.status == 200)
         callback(null, xhttp.responseText);
       else return callback(url_api);
     }
   };
-  xhttp.open('GET', url_api, false);
+  xhttp.open('GET', url_api, true);
   xhttp.send();
 };
 
-fetchData(API, function (error1, data1) {
+fetchData(API, (error1, data1) => {
   if (error1) return console.error('Error' + ' ' + error1);
   console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
+
+  data1 = JSON.parse( data1 )
+
+  let url = API + data1.results[0].id;
+
+  fetchData( url,  (error2, data2) => {
     if (error2) return console.error(error1);
     console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
+
+    data2 = JSON.parse( data2 )
+
+    fetchData(data2.origin.url,  (error3, data3) => {
       if (error3) return console.error(error3);
       console.log('Tercero Llamado...')
       console.log('Personajes:' + ' ' + data1.info.count);
@@ -29,4 +40,5 @@ fetchData(API, function (error1, data1) {
       console.log('Dimensión:' + ' ' + data3.dimension);
     });
   });
+
 });
