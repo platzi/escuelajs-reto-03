a81c5558334359532ea8ee215856b0c355132f23
Jefferson Aguilar <jeffersonaguilar@outlook.com>
2019-09-14 11:07:48 -0500

    Reto 3

--- src/index.js
+++ src/index.js
@@ -4,35 +4,39 @@ const API = 'https://rickandmortyapi.com/api/character/';
 const xhttp = new XMLHttpRequest();
 
 
-const fetchData = (url_api, callback) => {
-  xhttp.onreadystatechange = (event) => {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status === 200)
-        callback(null, xhttp.responseText);
-      else
-        return callback(url_api);
-    }
-  };
-  xhttp.open('GET', url_api, false);
-  xhttp.send();
+const fetchData = (url_api) => {
+  return new Promise((resolve, reject) => {
+    xhttp.onreadystatechange = (event) => {
+      if (xhttp.readyState === 4) {
+        if (xhttp.status === 200)
+          resolve(xhttp.responseText);
+        else
+          reject(url_api);
+      }
+    };
+    xhttp.open('GET', url_api, false);
+    xhttp.send();
+  });
+}
+
+const obtenerPersonaje = (id) => {
+  return fetchData(`${API}${id}`);
 }
 
 
-fetchData(API, (error1, data1) => {
-  if (error1) return console.error(`Error ${error1}`);
-  console.log('Primer Llamado...')
+const llamadoDatos = async () => {
+  console.log('Primer Llamado...');
+  let data1 = await fetchData(API);
   data1 = JSON.parse(data1);
-  fetchData(`${API}${data1.results[0].id}`, (error2, data2) => {
-    if (error2) return console.error(error1);
-    console.log('Segundo Llamado...')
-    data2 = JSON.parse(data2);
-    fetchData(data2.origin.url, (error3, data3) => {
-      data3 = JSON.parse(data3);
-      if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log(`Personajes: ${data1.info.count}`);
-      console.log(`Primer Personaje: ${data2.name}`);
-      console.log(`Dimensión: ${data3.dimension}`);
-    });
-  });
-});
+  console.log('Segundo Llamado...');
+  let data2 = await obtenerPersonaje(data1.results[0].id);
+  data2 = JSON.parse(data2);
+  let data3 = await fetchData(data2.origin.url);
+  data3 = JSON.parse(data3);
+  console.log('Tercero Llamado...')
+  console.log(`Personajes: ${data1.info.count}`);
+  console.log(`Primer Personaje: ${data2.name}`);
+  console.log(`Dimensión: ${data3.dimension}`);
+}
+
+llamadoDatos();
