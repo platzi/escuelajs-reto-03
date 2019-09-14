2dbc824e8bb67f19e7014b94bdffd2ea9b0b8ae8
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2019-09-14 12:48:45 -0500

    Se resulven los errores

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ var xhttp = new XMLHttpRequest();
 
 function fetchData(url_api, callback) {
   xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
-      if (xhttp.status == 200)
-        callback(null, xhttp.responseText);
-      else return callback(url_api);
+    if (xhttp.readyState === 4) {
+      if (xhttp.status == 200){
+        callback(null, JSON.parse(xhttp.responseText));
+      } else {
+        return callback(url_api);
+      }
     }
   };
   xhttp.open('GET', url_api, false);
