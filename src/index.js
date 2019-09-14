eb26779058d174e74fe8085405f872d13edc0d1f
Enrique Nieto Martínez <46055712+proartmateur@users.noreply.github.com>
2019-09-14 12:58:21 -0500

    Solve

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var API = 'https://rickandmortyapi.com/api/character/';
 var xhttp = new XMLHttpRequest();
 
-function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
+const fetchData = (url_api, callback) => {
+  
+  xhttp.onreadystatechange = (event) => {
+    if (xhttp.readyState === 4) {
       if (xhttp.status == 200)
         callback(null, xhttp.responseText);
       else return callback(url_api);
@@ -15,18 +16,39 @@ function fetchData(url_api, callback) {
   xhttp.send();
 };
 
-fetchData(API, function (error1, data1) {
-  if (error1) return console.error('Error' + ' ' + error1);
+var data_req_1 = Object;
+var data_req_2 = Object;
+
+const request3 = function (error3, data3) {
+  if (error3) return console.error(error3);
+  console.log('Tercero Llamado...')
+  console.log(`Tercero Lamando...`)
+
+
+  console.log(`Personajes ${data_req_1.info.count}`)
+  console.log(`Primer Personaje: ${data_req_2.name}`)
+  console.log(`Dimensión: ${data3.dimension}`)
+}
+
+const request2 = function (error2, data2) {
+  if (error2){
+    console.error(error1);
+  } 
+  data_req_2 = JSON.parse(data2)
+  console.log(data_req_2)
+  console.log('Segundo Llamado...')
+  console.log(data_req_2.origin)
+  fetchData(data_req_2.origin.url, request3);
+}
+
+const request1 = function (error1, data1) {
+
+  if (error1){
+    console.error('Error' + ' ' + error1);
+  } 
   console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
-    if (error2) return console.error(error1);
-    console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
-      if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
-    });
-  });
-});
+  data_req_1 = JSON.parse(data1)
+
+  fetchData(API + data_req_1.results[0].id, request2 );
+}
+fetchData(API, request1);
