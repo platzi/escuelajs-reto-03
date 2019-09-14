d9d3d0f885f552550f87431913f1266c399e387b
Raul Gomez <raul_nouni@hotmail.com>
2019-09-14 17:39:04 -0500

    Falta traducir a promesas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-var API   = 'https://rickandmortyapi.com/api/character/'
-var xhttp = new XMLHttpRequest()
+var API            = 'https://rickandmortyapi.com/api/character/'
+var xhttp          = new XMLHttpRequest()
 
 
 fetchData = (url_api, callback) => {
@@ -16,21 +15,21 @@ fetchData = (url_api, callback) => {
   xhttp.open('GET', url_api, false)
   xhttp.send()
 }
-
+// De momento no se como traducir esto a promesas o asyn await
 fetchData(API, (error1, data1) => {
   if (error1)
     return console.error(`Error1: ${error1}`)
   console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
+  fetchData(API + data1.results[0].id, (error2, data2) => {
     if (error2)
       return console.error(`Error 2: ${error2}`)
     console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
+    fetchData(data2.origin.url, (error3, data3) => {
       if (error3)
         return console.error(`Error 3: ${error3}`)
       console.log('Tercero Llamado...')
       console.log(`Personajes: ${data1.info.count}`)
-      console.log(`Primer personaje ${data2.name}`)
+      console.log(`Primer personaje: ${data2.name}`)
       console.log(`Dimension: ${data3.dimension}`)
     });
   });
