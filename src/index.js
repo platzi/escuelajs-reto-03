209794526b7724ebe3e0638cf7e4e8397737ba18
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2019-09-14 13:12:41 -0500

    fix third problem

--- src/index.js
+++ src/index.js
@@ -2,28 +2,30 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
 
 var API = "https://rickandmortyapi.com/api/character/"
 
-function fetchData(url_api, callback) {
-  var xhttp = new XMLHttpRequest()
-  xhttp.onreadystatechange = _ => {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status == 200) {
-        callback(null, JSON.parse(xhttp.responseText))
-      } else {
-        return callback(url_api)
+const fetchData = (url_api, callback = () => {}) => {
+  return new Promise((resolve, reject) => {
+    var xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = _ => {
+      if (xhttp.readyState === 4) {
+        if (xhttp.status == 200) {
+          resolve(callback(null, JSON.parse(xhttp.responseText)))
+        } else {
+          reject(callback(url_api))
+        }
       }
     }
-  }
-  xhttp.open("GET", url_api, true)
-  xhttp.send(null)
+    xhttp.open("GET", url_api, true)
+    xhttp.send(null)
+  })
 }
 
-fetchData(API, (error1, data1) => {
+fetchData(API, async (error1, data1) => {
   if (error1) return console.error(`Error ${error1}`)
   console.log("Primer Llamado...")
-  fetchData(`${API}${data1.results[0].id}`, (error2, data2) => {
+  await fetchData(`${API}${data1.results[0].id}`, async (error2, data2) => {
     if (error2) return console.error(`Error ${error2}`)
     console.log("Segundo Llamado...")
-    fetchData(data2.origin.url, (error3, data3) => {
+    await fetchData(data2.origin.url, (error3, data3) => {
       if (error3) return console.error(`Error ${error3}`)
       console.log("Tercero Llamado...")
       console.log(`Personajes: ${data1.info.count}`)
