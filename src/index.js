ec133c2ff5202e13bdce4cfba87715a5c92589a7
Edwin García <spark.com.co@gmail.com>
2019-09-14 18:18:15 -0500

    segunda parte

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var API = 'https://rickandmortyapi.com/api/character/';
 var xhttp = new XMLHttpRequest();
 
-function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = function (event) {
+const fetchData = (url_api, callback) => {
+  xhttp.onreadystatechange = event => {
     if (xhttp.readyState === 4) {
       if (xhttp.status == 200){
-        callback(null, xhttp.responseText);
+        callback(null, JSON.parse(xhttp.responseText));
       }else return callback(url_api);
     }
   }
@@ -15,21 +15,18 @@ function fetchData(url_api, callback) {
   xhttp.send();
 };
 
-fetchData(API, function (error1, data1) {
-  data1 = JSON.parse(data1)
-  if (error1) return console.error('Error' + ' ' + error1);
+fetchData(API, (error1, data1) => {
+  if (error1) return console.error(`Error ${error1}`);
   console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
-    data2 = JSON.parse(data2)
+  fetchData(`${API}${data1.results[0].id}`, (error2, data2) => {
     if (error2) return console.error(error1);
     console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
+    fetchData(data2.origin.url, (error3, data3) => {
       if (error3) return console.error(error3);
-      data3 = JSON.parse(data3)
       console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
+      console.log(`Personajes: ${data1.info.count}`)
+      console.log(`Primer personaje: ${data2.name}`);
+      console.log(`Dimensión: ${data3.dimension}`);
     });
   });
 });
