9f56d5d48ca052e3052ba8cd171cb7933d3a9530
Wilson Romero <wilson.romero@gmail.com>
2019-09-14 11:38:05 -0500

    tercer problema

--- src/index.js
+++ src/index.js
@@ -3,32 +3,41 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var API = "https://rickandmortyapi.com/api/character/";
 var xhttp = new XMLHttpRequest();
 
-function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = event => {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status == 200) callback(null, xhttp.responseText);
-      else return callback(url_api);
-    }
-  };
-  xhttp.open("GET", url_api, false);
-  xhttp.send();
-}
-
-fetchData(API, (error1, data1) => {
-  if (error1) return console.error("Error" + " " + error1);
-  console.log("Primer Llamado...");
-  data1 = JSON.parse(data1);
-  fetchData(API + data1.results[0].id, function(error2, data2) {
-    if (error2) return console.error(error1);
-    console.log("Segundo Llamado...");
-    data2 = JSON.parse(data2);
-    fetchData(data2.origin.url, function(error3, data3) {
-      if (error3) return console.error(error3);
-      console.log("Tercero Llamado...");
-      data3 = JSON.parse(data3);
-      console.log(`Personajes: ${data1.info.count}`);
-      console.log(`Primer Personaje: ${data2.name}`);
-      console.log(`Dimensión: ${data3.dimension}`);
-    });
+var fetchData = function(url_api, data) {
+  return new Promise(function(resolve, reject) {
+    xhttp.onreadystatechange = function() {
+      if (xhttp.readyState === 4) {
+        if (xhttp.status == 200) {
+          // If successful
+          resolve(xhttp);
+        } else {
+          // If failed
+          reject({
+            status: request.status,
+            statusJSON: request.statusText
+          });
+        }
+      }
+    };
+    xhttp.open("GET", url_api, false);
+    xhttp.send();
   });
-});
+};
+
+fetchData(API)
+  .then(data => {
+    console.log("Primer Llamado...");
+    data = JSON.parse(data.responseText);
+    console.log(`Personajes: ${data.info.count}`);
+    fetchData(API + data.results[0].id).then(data => {
+      data = JSON.parse(data.responseText);
+      console.log(`Primer Personaje: ${data.name}`);
+      fetchData(data.origin.url).then(data => {
+        data = JSON.parse(data.responseText);
+        console.log(`Dimensión: ${data.dimension}`);
+      });
+    });
+  })
+  .catch(function(error) {
+    console.log(`Error ${error}`);
+  });
