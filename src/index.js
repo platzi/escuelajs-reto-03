3b381f60f4726f51f223529e7207e8bbb0362d2c
Kristian Lopez <kristian.lom@gmail.com>
2019-09-14 19:57:08 -0500

    Se resuelve el tercer problema

--- src/index.js
+++ src/index.js
@@ -3,33 +3,31 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var API = `https://rickandmortyapi.com/api/character/`;
 var xhttp = new XMLHttpRequest();
 
-function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = (event) => {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status === 200)
-        callback(null, xhttp.responseText);
-      else return callback(url_api);
-    }
-  };
-  xhttp.open(`GET`, url_api, false);
-  xhttp.send();
+fetchData = (url_api) => {
+  return new Promise((resolve, reject) => {
+    xhttp.onreadystatechange = () => {
+      if (xhttp.readyState === 4 && xhttp.status === 200)
+        resolve(JSON.parse(xhttp.responseText));
+      else
+        reject(url_api)
+    };
+    xhttp.open(`GET`, url_api, false);
+    xhttp.send();
+
+  })
 };
 
-fetchData(API, (error1, data1) => {
-  if (error1) return console.error(`Error ${error1}`);
-  console.log(`Primer Llamado...`)
-  data1 = JSON.parse(data1)
-  fetchData(API + data1.results[0].id, (error2, data2) => {
-    if (error2) return console.error(error1);
-    console.log(`Segundo Llamado...`)
-    data2 = JSON.parse(data2)
-    fetchData(data2.origin.url, (error3, data3) => {
-      data3 = JSON.parse(data3)
-      if (error3) return console.error(error3);
-      console.log(`Tercero Llamado...`)
-      console.log(`Personajes: ${data1.info.count}`);
-      console.log(`Primer Personaje: ${data2.name}`);
-      console.log(`Dimensión: ${data3.dimension}`);
-    });
-  });
-});
+let infoCount = 0;
+let name = '';
+let dimension = '';
+
+fetchData(API)
+  .then(data => { console.log(`Primer Llamado...`); infoCount = data.info.count; return fetchData(API + data.results[0].id) })
+  .then(data => { console.log(`Segundo Llamado...`); name = data.name; return fetchData(data.origin.url) })
+  .then(data => {
+    console.log(`Tercero Llamado...`); dimension = data.dimension
+    console.log(`Personajes: ${infoCount}`);
+    console.log(`Primer Personaje: ${name}`);
+    console.log(`Dimensión: ${dimension}`);
+  })
+  .catch(error => { console.log(`Error: ${error}`) });
