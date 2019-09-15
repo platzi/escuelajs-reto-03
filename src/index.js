0a537e2a94f07d106250c459f574a592dda061c5
rjfm <javier.fuentesm@hotmail.com>
2019-09-15 01:13:49 -0500

    Se termianron los retos

--- src/index.js
+++ src/index.js
@@ -1,32 +1,53 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+//var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var API = 'https://rickandmortyapi.com/api/character/';
+var API = "https://rickandmortyapi.com/api/character/";
 var xhttp = new XMLHttpRequest();
+//Resolución d elso primeros dos problemas
 
 function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
-      if (xhttp.status == 200)
-        callback(null, xhttp.responseText);
+
+/*   function status(readyState) {
+    switch (readyState) {
+      case 0: return 'no inicializado'
+      case 1: return 'cargando...'
+      case 2: return 'cargado'
+      case 3: return 'interactivo'
+      case 4: return 'completado'        
+    }
+    
+  }
+ */
+
+  xhttp.open("GET", url_api, true);  
+  xhttp.send();
+  xhttp.onreadystatechange = even=> {
+    if (xhttp.readyState === 4) {
+      if (xhttp.status == 200) callback(null, JSON.parse(xhttp.responseText));
       else return callback(url_api);
     }
-  };
-  xhttp.open('GET', url_api, false);
-  xhttp.send();
-};
+  }; 
+  
+}
 
-fetchData(API, function (error1, data1) {
-  if (error1) return console.error('Error' + ' ' + error1);
-  console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
+function fetchDatapromise(url_api) {
+  return new Promise((resolve,reject)=>{
+
+  })
+  
+}
+
+fetchData(API, function(error1, data1) {
+  if (error1) return console.error("Error" + " " + error1);
+  console.log("Primer Llamado...");
+  fetchData(API + data1.results[0].id, function(error2, data2) {
     if (error2) return console.error(error1);
-    console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
+    console.log("Segundo Llamado...");
+    fetchData(data2.origin.url, function(error3, data3) {
       if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
+      console.log(`Tercero Llamado...`);
+      console.log(`Personajes: ${data1.info.count}`);
+      console.log(`Primer Personaje: ${data2.name}`);
+      console.log(`Dimensión: ${data3.dimension}`);
     });
   });
 });
---
+++ src/promesa.js
@@ -0,0 +1,23 @@
+var API = "https://rickandmortyapi.com/api/character/";
+//Resolucion del ejercicio 3 con fetch
+fetch(API).then(response => {
+  console.log("Primer Llamado...");
+  return response.json().then(data =>
+    fetch(API + data.results[0].id).then(data2 => {
+      console.log("Segundo Llamado...");
+
+      return data2.json().then(data3 => {
+        return fetch(data3.origin.url).then(data4 => {
+          console.log(`Tercero Llamado...`);
+
+          return data4.json().then(data5 => {
+            console.log(`Personajes: ${data.info.count}`);
+            console.log(`Primer Personaje: ${data3.name}`);
+            console.log(`Dimensión: ${data5.dimension}`);
+          });
+        });
+      });
+    })
+  );
+});
+
