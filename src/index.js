0ff4988e6794edfae66f68485214ecf9c54bd872
Rob <betoarpi@gmail.com>
2019-09-15 14:32:50 -0500

    Rob Arroyo betoarpi Ciudad de México Segundo Problema

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
-var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const API = 'https://rickandmortyapi.com/api/character/';
 
+function fetchData(url_api, callback) {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = () => {
+    if (xhttp.readyState === 4) {
+      if (xhttp.status == 200) {
+        const data = JSON.parse(xhttp.responseText);
+        callback(null, data);
+      } else {
+        return callback(xhttp.status);
+      }
+    }
+  };
+  xhttp.open('GET', url_api, false);
+  xhttp.send(null);
+}
+
+fetchData(API, (error1, data1) => {
+  if (error1) return console.error(`Error 1 ${error1}`);
+  console.log(`Primer llamado`);
+
+  fetchData(API + data1.results[0].id, (error2, data2) => {
+    if (error2) return console.error(error2);
+    console.log(`Segundo llamado`);
+
+    fetchData(data2.origin.url, (error3, data3) => {
+      if (error3) return console.error(error3);
+      console.log(`Tercero Llamado...`);
+      console.log(`Personajes: ${data1.info.count}`);
+      console.log(`Primer Personaje: ${data2.name}`);
+      console.log(`Dimensión: ${data3.dimension}`);
+    });
+  });
+});
+
+/* var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 var API = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url_api, callback) {
@@ -34,4 +70,4 @@ fetchData(API, function(error1, data1) {
       console.log('Dimensión:' + ' ' + data3.dimension);
     });
   });
-});
+}); */
