26af5f99949575f94a4dde7db861409809df8bfa
William Velazquez <34907456+WilliamVelazquez@users.noreply.github.com>
2019-09-14 23:22:46 -0500

    Solving 1st and 2nd Problem.
    -Fixing the code to work properly.
    -Updating the code with ECMAScript Syntaxis (Arrow Functions, Template Strings and Let/Const Variables).
    -Ignoring lock files.

--- src/index.js
+++ src/index.js
@@ -1,32 +1,40 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var API = 'https://rickandmortyapi.com/api/character/';
-var xhttp = new XMLHttpRequest();
+const API = 'https://rickandmortyapi.com/api/character/';
 
-function fetchData(url_api, callback) {
+const fetchData = (url_api, callback) => {
+  const xhttp = new XMLHttpRequest();
+  
   xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
-      if (xhttp.status == 200)
-        callback(null, xhttp.responseText);
+    // console.log('state-->', xhttp.readyState);
+    
+    if (xhttp.readyState === 4) {
+      // console.log('Status-->', xhttp.status);
+      if (xhttp.status == 200){
+        // console.log('-->',JSON.parse(xhttp.responseText));
+        callback(null, JSON.parse(xhttp.responseText));
+      }
       else return callback(url_api);
     }
   };
-  xhttp.open('GET', url_api, false);
+  xhttp.open('GET', url_api, true);
   xhttp.send();
 };
 
-fetchData(API, function (error1, data1) {
-  if (error1) return console.error('Error' + ' ' + error1);
+fetchData(API, (error1, data1) => {
+  // console.log('data1',JSON.parse(data1));
+  if (error1) return console.error(`Error ${error1}`);
   console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
-    if (error2) return console.error(error1);
+  // console.log('--->',data1.results);
+  fetchData(`${API}${data1.results[0].id}`, (error2, data2) => {
+    if (error2) return console.error(error2);
     console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
+    fetchData(data2.origin.url, (error3, data3) => {
       if (error3) return console.error(error3);
       console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
+      console.log(`Personajes: ${data1.info.count}`);
+      console.log(`Primer Personaje: ${data2.name}`);
+      console.log(`Dimensión: ${data3.dimension}`);
     });
   });
 });
