7ed164d69da39b870531c381248bdd00f2c0d37b
William Velazquez <34907456+WilliamVelazquez@users.noreply.github.com>
2019-09-15 17:15:24 -0500

    Using Promise.all to wait all the promises to be resolved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
+const COMPLETED_STATE = 4;
+const STATUS_OK = 200;
 const API = 'https://rickandmortyapi.com/api/character/';
 
 const fetchData = (url_api) => {
@@ -7,12 +9,10 @@ const fetchData = (url_api) => {
   return new Promise( 
     (resolve,reject) => {
       xhttp.onreadystatechange = (event) => {
-        if (xhttp.readyState === 4) {
-          if (xhttp.status == 200){
-            resolve(JSON.parse(xhttp.responseText));
-          }else{
-            reject(`URL ${url_api} NOT FOUND`);
-          }
+        if(xhttp.readyState === COMPLETED_STATE){
+          xhttp.status === STATUS_OK 
+          ? resolve(JSON.parse(xhttp.responseText))
+          : reject(`ERROR - URL: ${url_api}`);
         }
       }
       xhttp.open('GET', url_api, true);
@@ -21,19 +21,16 @@ const fetchData = (url_api) => {
   );
 };
 
-fetchData(API)
-  .then(data1 => {
-    console.log('Primer Llamado...');
-    console.log(`Personajes: ${data1.info.count}`);
-    return fetchData(`${API}${data1.results[0].id}`);
-  })
-  .then(data2 => {
-    console.log('Segundo Llamado...')
-    console.log(`Primer Personaje: ${data2.name}`);
-    return fetchData(data2.origin.url);
-  })
-  .then(data3 => {
-    console.log('Tercero Llamado...')
-    console.log(`Dimensión: ${data3.dimension}`);
-  })
-  .catch(error => console.log(`Error: ${error}`));
+const promiseA = fetchData(API);
+const promiseB = promiseA.then(data1 => fetchData(`${API}${data1.results[0].id}`));
+const promiseC = promiseB.then(data2 => fetchData(data2.origin.url));
+
+Promise.all([promiseA,promiseB,promiseC])
+.then(([dataA, dataB, dataC]) => { 
+  console.log(`Personajes: ${dataA.info.count}`);
+  console.log(`Primer Personaje: ${dataB.name}`);
+  console.log(`Dimensión: ${dataC.dimension}`);
+})
+.catch(error => { 
+  console.log(error)
+});
