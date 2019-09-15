7fc142f460f9b5bcc02b13b8cfcecb687e5d3e2f
David R <deveeup@MacBook-Pro.local>
2019-09-14 19:43:34 -0500

    reutilizable function

--- src/index.js
+++ src/index.js
@@ -17,23 +17,47 @@ const fetchData = (url_api, callback) => {
   xhr.send();
 };
 
+const jsonString = (data, param) => {
+  const response = JSON.parse(data);
+  let searchParam, proxUrl;
+  switch (param) {
+    case "search_id":
+      searchParam = response.results[0].id;
+      proxUrl = `${BASE_API}${searchParam}`;
+      return { response, proxUrl };
+      break;
+    case "url_person":
+      searchParam = response.origin.url;
+      proxUrl = searchParam;
+      return { data, proxUrl };
+      break;
+    case "finally_info":
+      return {
+        name: response.name,
+        type: response.type,
+        dimension: response.dimension
+      };
+    default:
+      return null;
+  }
+};
+
+const errorFunction = () => {
+  return `error: ${error_log}`;
+};
+
 fetchData(BASE_API, (error_one, data_one) => {
-  if (error_one) return console.error(`error: ${error_one}`);
-  const convertJSON = JSON.parse(data_one),
-    idPerson = convertJSON.results[0].id,
-    API_PERSON = `${BASE_API}${idPerson}`;
+  if (error_one) errorFunction(error_one);
+  const res = jsonString(data_one, "search_id");
 
-  fetchData(API_PERSON, (error_two, data_two) => {
-    if (error_two) return console.error(`error: ${error_two}`);
-    const convertJSON2 = JSON.parse(data_two),
-      URL_PERSON = convertJSON2.origin.url;
+  fetchData(res.proxUrl, (error_two, data_two) => {
+    if (error_two) errorFunction(error_two);
+    const res2 = jsonString(data_two, "url_person");
 
-    fetchData(URL_PERSON, (error_three, data_three) => {
-      if (error_three) return console.error(`error: ${error_three}`);
-      const person = JSON.parse(data_three);
-      console.log(`Person: ${person.name}`);
-      console.log(`Dimension: ${person.dimension}`);
-      console.log(`Planet: ${person.type}`);
+    fetchData(res2.proxUrl, (error_three, data_three) => {
+      if (error_three) errorFunction(error_three);
+      const res3 = jsonString(data_three, "finally_info");
+      console.log(res3);
     });
   });
 });
