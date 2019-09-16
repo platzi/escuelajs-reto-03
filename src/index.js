e4ada886b1d1ff795164f7fe0254f3f49f28cece
David R <jossedaviid1@gmail.com>
2019-09-16 14:20:09 -0500

    3. Promises

--- src/index.js
+++ src/index.js
@@ -3,61 +3,46 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const BASE_API = "https://rickandmortyapi.com/api/character/",
   xhr = new XMLHttpRequest();
 
-const fetchData = (url_api, callback) => {
-  xhr.open("GET", url_api, false);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        return callback(null, xhr.responseText);
-      } else {
-        console.log("error!");
+const fetchData = url => {
+  xhr.open('GET', url, false)
+  xhr.responseType = 'json'
+  return new Promise((res, rej) => {
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          const response = JSON.parse(xhr.responseText)
+          return res(response)
+        } else {
+          return rej(new Error('SORRY! ERROR HAS OCURRED :('))
+        }
       }
     }
-  };
-  xhr.send();
-};
+    xhr.send()
+  })
+}
 
-const jsonString = (data, param) => {
-  const response = JSON.parse(data);
-  let searchParam, proxUrl;
-  switch (param) {
-    case "search_id":
-      searchParam = response.results[0].id;
-      proxUrl = `${BASE_API}${searchParam}`;
-      return { response, proxUrl };
-      break;
-    case "url_person":
-      searchParam = response.origin.url;
-      proxUrl = searchParam;
-      return { data, proxUrl };
-      break;
-    case "finally_info":
-      return {
-        name: response.name,
-        type: response.type,
-        dimension: response.dimension
-      };
-    default:
-      return null;
-  }
-};
+const searchData  = async url => {
+  try {
+    console.log('L O A D I N G')
+    const world = await fetchData(url)
 
-const errorFunction = () => {
-  return `error: ${error_log}`;
-};
+    console.log('L O A D I N G . . .')
+    const chapters = await fetchData(`${BASE_API}${world.results[0].id}`)
 
-fetchData(BASE_API, (error_one, data_one) => {
-  if (error_one) errorFunction(error_one);
-  const res = jsonString(data_one, "search_id");
+    console.log('L O A D I N G . . . . . .')
+    const characters = await fetchData(`${chapters.origin.url}`)
 
-  fetchData(res.proxUrl, (error_two, data_two) => {
-    if (error_two) errorFunction(error_two);
-    const res2 = jsonString(data_two, "url_person");
+    console.log(' ')
+    console.log('------------------------------')
+    console.log(' ')
 
-    fetchData(res2.proxUrl, (error_three, data_three) => {
-      if (error_three) errorFunction(error_three);
-      const res3 = jsonString(data_three, "finally_info");
-      console.log(res3);
-    });
-  });
-});
+    console.log(`1. Count characters: ${world.info.count}`)
+    console.log(`2. First character: ${chapters.name}`)
+    console.log(`3. Dimensions: ${characters.dimension}`)
+
+    }catch(error) {
+      console.log(error.text)
+    }
+}
+
+searchData(BASE_API)
