da4ead9db44462ea26f96ec0d30b7c22583c1254
César Hansy Dueñas Barragán <cesarhansy@gmail.com>
2019-09-18 23:01:29 -0500

    Works with Promises

--- src/index.js
+++ src/index.js
@@ -18,15 +18,18 @@ let xhttp = new XMLHttpRequest();
 //   xhttp.send();
 // };
 
-function fetchData (url_api, callback){
+const fetchData = url_api =>{
   return new Promise( (resolve, reject) => {
     xhttp.onreadystatechange = (event) => {
-      if (xhttp.readyState === 4) {
-        if (xhttp.status === 200) {
-          resolve(JSON.parse(xhttp.responseText));
-        }
+      if (xhttp.readyState === 4 && xhttp.status === 200) {
+        return resolve(JSON.parse(xhttp.responseText))
+      }
+      else {
+        return reject (`Error when execute: ${url_api}`)
       }
     }
+    xhttp.open('GET', url_api, false)
+    xhttp.send();
   })
 }
 
@@ -37,16 +40,16 @@ function onError(error){
 fetchData(API)
   .then (data1 => {
     console.log('Primer Llamado...')
+    console.log(`Personajes: ${data1.info.count}`);
     return fetchData(`${API} ${data1.results[0].id}`)
   })
   .then (data2 => {
     console.log('Segundo Llamado...')
+    console.log(`Primer Personaje: ${data2.name}`);
     return fetchData(data2.origin.url)
   })
   .then (data3 => {
     console.log('Tercero Llamado...')
-    console.log(`Personajes: ${data1.info.count}`);
-    console.log(`Primer Personaje: ${data2.name}`);
     console.log(`Dimensión: ${data3.dimension}`);
   })
   .catch(onError)
