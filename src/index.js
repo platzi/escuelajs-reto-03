e6c74e168f29b8fab8200cb421551efbd69b5b73
Francisco Angel <paconity@gmail.com>
2019-09-19 13:19:40 -0500

    Fin del reto

--- src/index.js
+++ src/index.js
@@ -2,31 +2,39 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 var API = 'https://rickandmortyapi.com/api/character/';
 var xhttp = new XMLHttpRequest();
+var dataBox = [];
 
-function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
-      if (xhttp.status == 200)
-        callback(null, xhttp.responseText);
-      else return callback(url_api);
+function fetchData(url_api, data){
+  return new Promise((resolve, reject) => {
+    xhttp.onreadystatechange = (event) =>{
+      if (xhttp.readyState === 4 && xhttp.status == 200){
+        resolve(JSON.parse(xhttp.responseText), data);
+      } else return reject(url_api);
     }
-  };
-  xhttp.open('GET', url_api, false);
-  xhttp.send();
-};
+    xhttp.open('GET', url_api, false);
+    xhttp.send();
+  })  
+}
 
-fetchData(API, function (error1, data1) {
-  if (error1) return console.error('Error' + ' ' + error1);
-  console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
-    if (error2) return console.error(error1);
-    console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
-      if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
-    });
-  });
-});
+fetchData(API).then((data1) => {
+  console.log(`Primer Llamado...`);
+  dataBox.push(data1);
+  return fetchData(API + data1.results[0].id, data1);
+}).catch((error1) => {
+  console.error(`Error ${error1}`);
+})
+.then((data2) => {
+  console.log(`Segundo Llamado...`);
+  dataBox.push(data2);
+  return fetchData(data2.origin.url);
+}).catch((error2) => {
+  console.error(`${error2}`);
+})
+.then((data3) => {
+  console.log(`Tercer Llamado...`);
+  console.log(`Personajes: ${dataBox[0].info.count}`);
+  console.log(`Primer Personaje: ${dataBox[1].name}`);
+  console.log(`Dimensión: ${data3.dimension}`);
+}).catch((error3) => {
+  console.error(`${error3}`);
+})
