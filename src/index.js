a8b4e61fcbd5da369077bf3e5feb0fe3dd7c9206
solid94 <davisolar94@gmail.com>
2019-09-19 22:33:57 -0500

    Problema 2 solucionado

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var API = "https://rickandmortyapi.com/api/character/";
 var xhttp = new XMLHttpRequest();
 
 function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = function(event) {
+  xhttp.onreadystatechange = event => {
     // comprobar que tipo de dato regresa
     //console.log("tipo de dato: ", xhttp.readyState);
     //cambiamos a 4 entero ya que el === comprueba el tipo de dato
@@ -20,22 +20,22 @@ function fetchData(url_api, callback) {
   xhttp.send();
 }
 
-fetchData(API, function(error1, data1) {
-  if (error1) return console.error("Error" + " " + error1);
+fetchData(API, (error1, data1) => {
+  if (error1) return console.error(`Error ${error1}`);
   console.log("Primer Llamado...");
   // para poser extraer la info, lo convertimos a un objeto JSON
   //data1 = JSON.parse(data1);
   //console.log(data1.results[0]);
-  fetchData(API + data1.results[0].id, function(error2, data2) {
+  fetchData(API + data1.results[0].id, (error2, data2) => {
     if (error2) return console.error(error1);
     console.log("Segundo Llamado...");
     //data2 = JSON.parse(data2);
-    fetchData(data2.origin.url, function(error3, data3) {
+    fetchData(data2.origin.url, (error3, data3) => {
       if (error3) return console.error(error3);
       console.log("Tercero Llamado...");
-      console.log("Personajes:" + " " + data1.info.count);
-      console.log("Primer Personaje:" + " " + data2.name);
-      console.log("Dimensión:" + " " + data3.dimension);
+      console.log(`Personajes: ${data1.info.count}`);
+      console.log(`Primer Personaje: ${data2.name}`);
+      console.log(`Dimensión: ${data3.dimension}`);
     });
   });
 });
