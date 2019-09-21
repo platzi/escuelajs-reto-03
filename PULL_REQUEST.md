c8db786175b8e7b0bb178bf99824fda4850d1801
lisset-orozco <liss612@gmail.com>
2019-09-20 23:43:20 -0500

    Reto 3

---
+++ PULL_REQUEST.md
@@ -0,0 +1,15 @@
+## DESCRIPTION
+
+Nombre: Lisset Orozco
+Usuario Platzi: @lisset612
+
+## Ciudad
+
+- [x] Ciudad de México
+- [ ] Bogotá
+
+# Retos:
+
+- [ ] Primer problema
+- [ ] Segundo problema
+- [x] Tercer problema
--- src/index.js
+++ src/index.js
@@ -1,32 +1,95 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const API = "https://rickandmortyapi.com/api/character/";
 
-var API = 'https://rickandmortyapi.com/api/character/';
-var xhttp = new XMLHttpRequest();
+// ***************************** CALLBACK HELL
 
-function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
-      if (xhttp.status == 200)
-        callback(null, xhttp.responseText);
+const fetchData1 = (url_api, callback) => {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(event) {
+    if (xhttp.readyState === 4) {
+      if (xhttp.status == 200) callback(null, JSON.parse(xhttp.responseText));
       else return callback(url_api);
     }
   };
-  xhttp.open('GET', url_api, false);
+  xhttp.open("GET", url_api, true);
   xhttp.send();
 };
 
-fetchData(API, function (error1, data1) {
-  if (error1) return console.error('Error' + ' ' + error1);
-  console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
+fetchData1(API, function(error1, data1) {
+  if (error1) return console.error(`Error ${error1}`);
+  console.log("Primer Llamado...");
+  fetchData1(API + data1.results[0].id, function(error2, data2) {
     if (error2) return console.error(error1);
-    console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
+    console.log("Segundo Llamado...");
+    fetchData1(data2.origin.url, function(error3, data3) {
       if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
+      console.log("Tercero Llamado...");
+      console.log("***************************** CALLBACK HELL");
+      console.log(`Personajes: ${data1.info.count}`);
+      console.log(`Primer Personaje: ${data2.name}`);
+      console.log(`Dimensión: ${data3.dimension}`);
+      console.log("\n");
     });
   });
 });
+// // ***************************** PROMESAS
+
+const fetchData2 = url_api => {
+  const xhttp = new XMLHttpRequest();
+  return new Promise((resolve, reject) => {
+    xhttp.onreadystatechange = function(event) {
+      if (xhttp.readyState === 4) {
+        if (xhttp.status == 200) return resolve(JSON.parse(xhttp.responseText));
+        else return reject(url_api);
+      }
+    };
+    xhttp.open("GET", url_api, true);
+    xhttp.send(null);
+  });
+};
+
+let results = {};
+
+fetchData2(API)
+  .then(data1 => {
+    results.data1 = data1;
+    console.log("Primer Llamado...");
+    return fetchData2(API + data1.results[0].id);
+  })
+  .then(data2 => {
+    results.data2 = data2;
+    console.log("Segundo Llamado...");
+    return fetchData2(data2.origin.url);
+  })
+  .then(data3 => {
+    console.log("Tercero Llamado...");
+    console.log("***************************** PROMESAS");
+    console.log(`Personajes: ${results.data1.info.count}`);
+    console.log(`Primer Personaje: ${results.data2.name}`);
+    console.log(`Dimensión: ${data3.dimension}`);
+    console.log("\n");
+  })
+  .catch(error => {
+    console.log("Error");
+    console.error(error);
+  });
+
+// // ***************************** ASYNC AWAIT
+const asyncAwait = async () => {
+  try {
+    console.log("calling");
+    const data1 = await fetchData2(API);
+    const data2 = await fetchData2(API + data1.results[0].id);
+    const data3 = await fetchData2(data2.origin.url);
+
+    console.log("***************************** ASYNC AWAIT");
+    console.log(`Personajes: ${results.data1.info.count}`);
+    console.log(`Primer Personaje: ${results.data2.name}`);
+    console.log(`Dimensión: ${data3.dimension}`);
+    console.log("\n");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+asyncAwait();
