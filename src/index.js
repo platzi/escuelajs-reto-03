bfb6cc3de476103c924361eeaa07ef71a08428ff
Alexis Araujo <ala1289@gmail.com>
2019-09-20 20:55:07 -0500

    Se realizaron los cambios propuestos por @gndx
    -Se eliminaron los comentarios
    -Se cambio los nombres de las funciones a ingles (por convencion nuestro codigo debe estar en ingles)
    -Se especifica en el reject el error

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 const API = 'https://rickandmortyapi.com/api/character/';
-const request = new XMLHttpRequest()
-// Reto 3 resolviendo reto con promesas
+const request = new XMLHttpRequest();
 
-const fetchData = (url_api) => {
-  return new Promise((sePudo, todoMal) =>{
+const fetchData = url_api => {
+  return new Promise((resolve, reject) => {
     request.onreadystatechange = () => {
       if (request.readyState === 4) {
-        if (request.status == 200)
-        sePudo(JSON.parse(request.responseText));
-        else return todoMal(url_api);
+        if (request.status == 200) resolve(JSON.parse(request.responseText));
+        else return reject(new Error(`${request.status}`));
       }
     };
     request.open('GET', url_api, false);
     request.send();
-  })
-}
+  });
+};
 
 fetchData(API)
   .then(data => {
@@ -31,10 +29,9 @@ fetchData(API)
   })
   .then(data => {
     data3 = data;
-    console.log(`Tercer llamado...`)
+    console.log(`Tercer llamado...`);
     console.log(`Personajes: ${data1.info.count}`);
     console.log(`Primer personaje: ${data2.name}`);
     console.log(`Dimensión: ${data3.dimension}`);
   })
-  .catch(`Error`)
-
+  .catch(`Error`);
