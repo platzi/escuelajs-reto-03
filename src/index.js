909751cab5273728787ba83ca125254bbb11b014
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2019-09-23 03:13:43 -0500

    Fix comments

--- src/index.js
+++ src/index.js
@@ -1,42 +1,44 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
 
-var API = "https://rickandmortyapi.com/api/character/"
+const API = "https://rickandmortyapi.com/api/character/"
 
-const fetchData = (url_api, callback = () => {}) => {
+const fetchData = (url_api = API, data = {}) => {
   return new Promise((resolve, reject) => {
-    var xhttp = new XMLHttpRequest()
-    xhttp.onreadystatechange = _ => {
+    const xhttp = new XMLHttpRequest()
+
+    xhttp.onreadystatechange = () => {
       if (xhttp.readyState === 4) {
-        if (xhttp.status == 200) {
-          resolve(callback(null, JSON.parse(xhttp.responseText)))
+        if (xhttp.status === 200) {
+          resolve({ response: JSON.parse(xhttp.responseText), data })
         } else {
-          reject(callback(url_api))
+          reject(`Error to fetch: ${url_api}`)
         }
       }
     }
+
     xhttp.open("GET", url_api, true)
     xhttp.send(null)
   })
 }
 
-fetchData(API, (error1, data1) => {
-  if (error1) console.error(error1)
-  console.log("Primer Llamado...")
-  return data1
-})
-  .then(res =>
-    fetchData(`${API}${res.results[0].id}`, (error2, data2) => {
-      if (error2) console.error(error2)
-      console.log("Segundo Llamado...")
-      return { data1: res, data2 }
+fetchData()
+  .then(data1 => {
+    console.log("Primer Llamado...")
+    return fetchData(`${API}${data1.response.results[0].id}`, {
+      data1: data1.response
     })
-  )
-  .then(res =>
-    fetchData(res.data2.origin.url, (error3, data3) => {
-      if (error3) return console.error(`Error ${error3}`)
-      console.log("Tercer Llamado...")
-      console.log(`Personajes: ${res.data1.info.count}`)
-      console.log(`Primer Personaje: ${res.data2.name}`)
-      console.log(`Dimensión: ${data3.dimension}`)
+  })
+  .then(data2 => {
+    console.log("Segundo Llamado...")
+    return fetchData(data2.response.origin.url, {
+      ...data2.data,
+      data2: data2.response
     })
-  )
+  })
+  .then(data3 => {
+    console.log("Tercer Llamado...")
+    console.log(`Personajes: ${data3.data.data1.info.count}`)
+    console.log(`Primer Personaje: ${data3.data.data2.name}`)
+    console.log(`Dimensión: ${data3.response.dimension}`)
+  })
+  .catch(err => console.error(err))
