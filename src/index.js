ebe064da0b9efafe870b1468b20e716d0718e23b
Santiago Jimenez <sjimen13@gmail.com>
2019-09-23 10:32:31 -0500

    reto3

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ var xhttp = new XMLHttpRequest();
 
 function fetchData(url_api, callback) {
   xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
+    if (xhttp.readyState === 4) {
       if (xhttp.status == 200)
         callback(null, xhttp.responseText);
       else return callback(url_api);
@@ -17,8 +17,10 @@ function fetchData(url_api, callback) {
 
 fetchData(API, function (error1, data1) {
   if (error1) return console.error('Error' + ' ' + error1);
-  console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
+  console.log('Primer Llamado...');
+  const dato1 = JSON.parse(data1);
+  console.log(dato1);
+  fetchData(API + dato1.results[0].id, function (error2, data2) {
     if (error2) return console.error(error1);
     console.log('Segundo Llamado...')
     fetchData(data2.origin.url, function (error3, data3) {
