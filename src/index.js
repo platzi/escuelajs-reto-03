0a135c5955caaa19f1f9e1c5204c16ced2abb673
Luis Caraballo <luisred61@gmail.com>
2019-09-24 10:24:22 -0500

    Se agregan correcciones segun comentarios de Pull Request

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var API = 'https://rickandmortyapi.com/api/character/';
-var xhttp = new XMLHttpRequest();
-var storage = []
+const API = `https://rickandmortyapi.com/api/character/`;
+let xhttp = new XMLHttpRequest();
+let storage = []
 
 
 const fetchData = url_api => {
-  return new Promise((resolve, reject) => {
+  return new Promise ( (resolve, reject) => {
       xhttp.onreadystatechange =  event => {
         if (xhttp.readyState === 4) {
           if (xhttp.status == 200)
             resolve(JSON.parse(xhttp.responseText));
-          else reject(url_api);
+          else reject(`Existen problemas con la url: ${url_api}`);
         }
       };
       xhttp.open('GET', url_api, false);
@@ -22,21 +22,21 @@ const fetchData = url_api => {
 fetchData(API)
   .then(data1 => {
     storage.push(data1);  
-    console.log ("Primer Llamado...")
+    console.log(`Primer Llamado...`)
     return fetchData(API+data1.results[0].id)
   })
-  .then(data2 =>{
+  .then(data2 => {
     storage.push(data2)
-    console.log ("Segundo Llamado...")
+    console.log(`Segundo Llamado...`)
     return fetchData(data2.origin.url)
   })
-  .then(data3 =>{
+  .then(data3 => {
     storage.push(data3)
     console.log("Tercer LLamado...")
     console.log(`Personajes: ${storage[0].info.count}`)
     console.log(`Primer Personaje: ${storage[1].name}`)
     console.log(`Dimension: ${storage[2].dimension}`)
   })
-  .catch(Error =>{ 
-    console.log("Se ha producido un error...")
+  .catch(Error => { 
+    console.log(`Se ha producido un error...`)
   })
