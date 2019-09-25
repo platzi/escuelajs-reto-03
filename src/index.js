8e53cbcd0b033727db57ea2f8a4c2e63607f8870
Santiago Jimenez <sjimen13@gmail.com>
2019-09-25 08:33:49 -0500

    bug fixed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ var xhttp = new XMLHttpRequest();
 
 
 function fetchData(url_api, callback) {
-  xhttp.onreadystatechange =  (event) => {
+  xhttp.onreadystatechange =  () => {
     if (xhttp.readyState === 4) {
       if (xhttp.status == 200)
         callback(null, xhttp.responseText);
@@ -19,7 +19,7 @@ function fetchData(url_api, callback) {
 
 const information = async()=> {
 //  try{
-  
+  try{
   let datos1; 
   await fetchData(API,  (error1, data1) => {
     datos1 = JSON.parse(data1);
@@ -33,34 +33,21 @@ const information = async()=> {
     return datos2;
   });
   console.log('Segundo  Llamado...');
-  console.log(datos2)
-  // let datos3; 
-  // await fetchData(API+datos2.origin.url,  (error1, data3) => {
-  //   datos3 = JSON.parse(data3);
-  //   return datos3;
-  // });
-  // console.log(datos3)
   
+  let datos3; 
+  await fetchData(datos2.origin.url,  (error1, data3) => {
+    datos3 = JSON.parse(data3);
+    return datos3;
+  });
+  console.log('Tercer  Llamado...');
+  console.log('El numero de Personajes son:' + ' ' + datos1.info.count);
+  console.log('Primer Personaje:' + ' ' + datos2.name);
+  console.log('Dimensión:' + ' ' + datos3.dimension);
+  }
+  catch(error) {
+    console.log('Tenemos un error en los llamados')
+  }
 }
 
 information()
 
-// fetchData(API,  (error1, data1) => {
-//   console.log('Primer Llamado...');
-//   const dato1 = JSON.parse(data1);
-
-//   fetchData(API + dato1.results[0].id, function (error2, data2) {
-//     const dato2 = JSON.parse(data2);
-//     if (error2) return console.error(error1);
-//     console.log('Segundo Llamado...')
-
-//     fetchData(dato2.origin.url, function (error3, data3) {
-//       const dato3 = JSON.parse(data3);
-//       if (error3) return console.error(error3);
-//       console.log('Tercero Llamado...')
-//       console.log('Personajes:' + ' ' + dato1.info.count);
-//       console.log('Primer Personaje:' + ' ' + dato2.name);
-//       console.log('Dimensión:' + ' ' + dato3.dimension);
-//     });
-//   });
-// })
