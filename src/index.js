f61c7f094db83d0dd93c0fb7e7b2598db8846f4f
Bryson Florez <brysonsaman@hotmail.com>
2020-06-15 19:05:48 -0500

    Se realizan los puntos del segundo problema

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var API = 'https://rickandmortyapi.com/api/character/'
+var API = 'https://rickandmortyapi.com/api/character/';
 
-function fetchData(url_api, callback) {
+const fetchData = (url_api, callback) => {
   var xhttp = new XMLHttpRequest();
   xhttp.open('GET', url_api, true);
-  xhttp.onreadystatechange = function (event) {
+  xhttp.onreadystatechange = (event) => {
     if (xhttp.readyState === 4) {
       if (xhttp.status == 200)
         callback(null, JSON.parse(xhttp.responseText));
@@ -16,17 +16,17 @@ function fetchData(url_api, callback) {
 };
 
 fetchData(API, function (error1, data1) {
-  if (error1) return console.error('Error' + ' ' + error1);
-  console.log('Primer Llamado...')
+  if (error1) return console.error(`Error ${error1}`);
+  console.log('Primer Llamado...');
   fetchData(API + data1.results[0].id, function (error2, data2) {
     if (error2) return console.error(error1);
-    console.log('Segundo Llamado...')
+    console.log('Segundo Llamado...');
     fetchData(data2.origin.url, function (error3, data3) {
       if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
+      console.log('Tercero Llamado...');
+      console.log(`Personajes: ${data1.info.count}`);
+      console.log(`Primer Personaje: ${data2.name}`);
+      console.log(`Dimensión: ${data3.dimension}`);
     });
   });
 });
