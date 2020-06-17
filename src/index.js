f825c8789e7bce8204a339fff37581610e3be4dc
Bryson Florez <brysonsaman@hotmail.com>
2020-06-16 21:19:20 -0500

    Se pasa a template strings

--- src/index.js
+++ src/index.js
@@ -17,13 +17,13 @@ const fetchData = (url_api, callback) => {
 
 fetchData(API, (error1, data1) => {
   if (error1) return console.error(`Error ${error1}`);
-  console.log('Primer Llamado...');
+  console.log(`Primer Llamado...`);
   fetchData(API + data1.results[0].id, (error2, data2) => {
     if (error2) return console.error(error1);
-    console.log('Segundo Llamado...');
+    console.log(`Segundo Llamado...`);
     fetchData(data2.origin.url, (error3, data3) => {
       if (error3) return console.error(error3);
-      console.log('Tercero Llamado...');
+      console.log(`Tercero Llamado...`);
       console.log(`Personajes: ${data1.info.count}`);
       console.log(`Primer Personaje: ${data2.name}`);
       console.log(`Dimensión: ${data3.dimension}`);
