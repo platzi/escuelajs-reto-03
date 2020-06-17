a0e81ae5e2a2bb8fa9cf1c1c07ee3d9cb082a602
Bryson Florez <brysonsaman@hotmail.com>
2020-06-17 15:40:25 -0500

    Se realiza la conversion a Promesas

--- src/index.js
+++ src/index.js
@@ -2,31 +2,29 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 var API = 'https://rickandmortyapi.com/api/character/';
 
-const fetchData = (url_api, callback) => {
+const fetchData = (url_api) => {
   var xhttp = new XMLHttpRequest();
-  xhttp.open('GET', url_api, true);
-  xhttp.onreadystatechange = (event) => {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status == 200)
-        callback(null, JSON.parse(xhttp.responseText));
-      else return callback(url_api);
-    }
-  };
-  xhttp.send();
-};
-
-fetchData(API, (error1, data1) => {
-  if (error1) return console.error(`Error ${error1}`);
-  console.log(`Primer Llamado...`);
-  fetchData(API + data1.results[0].id, (error2, data2) => {
-    if (error2) return console.error(error1);
-    console.log(`Segundo Llamado...`);
-    fetchData(data2.origin.url, (error3, data3) => {
-      if (error3) return console.error(error3);
-      console.log(`Tercero Llamado...`);
-      console.log(`Personajes: ${data1.info.count}`);
-      console.log(`Primer Personaje: ${data2.name}`);
-      console.log(`Dimensión: ${data3.dimension}`);
-    });
+  return new Promise((resolve, reject) => {
+    xhttp.open('GET', url_api, true);
+    xhttp.onreadystatechange = (event) => {
+      if (xhttp.readyState === 4) {
+        if (xhttp.status == 200)
+          resolve(JSON.parse(xhttp.responseText));
+        else return reject(url_api);
+      }
+    };
+    xhttp.send();
   });
-});
+}
+
+fetchData(API)
+  .then(response => {
+    console.log(`${response.info.count}`);
+    return fetchData(`${API}${response.results[0].id}`);
+  })
+  .then(response => {
+    console.log(response.name)
+    return fetchData(`${response.origin.url}`)
+  })
+  .then(response => console.log(response.dimension))
+  .catch(err => console.log(err));
