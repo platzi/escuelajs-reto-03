66ca29c984ddee885e9befd56dada27583302d0c
HarpeBlue <hspe2507@gmail.com>
2020-07-22 17:51:00 -0500

    Solución tercer problema, evitar Callback Hell con ayúda de promesas.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,34 @@
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const API = 'https://rickandmortyapi.com/api/character/';
 
-function fetchData(url_api, callback) {
-  const xhttp = new XMLHttpRequest();
-  xhttp.open('GET', url_api, true);
-  xhttp.onreadystatechange = event => {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status === 200) {
-        callback(null, JSON.parse(xhttp.responseText));
-      } else {
-        return callback(url_api);
+function fetchData(url_api) {
+  return new Promise((resolve, reject) => {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url_api, true);
+    xhttp.onreadystatechange = event => {
+      if (xhttp.readyState === 4) {
+        (xhttp.status === 200)
+        ? resolve(JSON.parse(xhttp.responseText))
+        : reject(new Error('error ', url_api));
       }
-    }
-  };
-  xhttp.send();
+    };
+    xhttp.send();
+  })
 };
 
-fetchData(API, (error1, data1) => {
-  if (error1) return console.error(`Error ${error1}`);
-  console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, (error2, data2) => {
-    if (error2) return console.error(error1);
-    console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, (error3, data3) => {
-      if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log(`Personajes: ${data1.info.count}`);
-      console.log(`Primer Personaje: ${data2.name}`);
-      console.log(`Origen: ${data3.dimension}`);
-    });
-  });
-});
+fetchData(API)
+  .then(data1 => {
+    console.log('Primer Llamado...')
+    console.log(`Personajes: ${data1.info.count}`);
+    return fetchData(`${API}${data1.results[0].id}`)
+  })
+  .then(data2 => {
+    console.log('Segundo llamado...')
+    console.log(`Primer Personaje: ${data2.name}`);
+    return fetchData(data2.origin.url)
+  })
+  .then(data3 => {
+    console.log('Tercer llamado...')
+    console.log(`Origen: ${data3.dimension}`);
+  })
+  .catch(err => console.error(err))
