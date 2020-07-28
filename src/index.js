ca1eec76b2fe766d5fce1c4b0eb3c0ed13e68928
Max23Esau <esa-max23@live.com>
2020-07-28 07:08:16 -0500

    add promises

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 let API = 'https://rickandmortyapi.com/api/character/';
 
-function fetchData(url_api, callback) {
-  let xhttp = new XMLHttpRequest();
-  xhttp.open('GET', url_api, true);
-  xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status == 200) {
-        callback(null, JSON.parse(xhttp.responseText));
-      } else {
-        const error = new Error('Error ' + url_api);
-        return callback(error, null);
+const fetchData = (url_api) => {
+  return new Promise((resolve, reject) => {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url_api, true);
+    xhttp.onreadystatechange = (() => {
+      if (xhttp.readyState === 4) {
+        (xhttp.status === 200)
+          ? resolve(JSON.parse(xhttp.responseText))
+          : reject(new Error('Error ', url_api))
       }
-    }
-  };
-  xhttp.send();
-};
-
-fetchData(API, function (error1, data1) {
-  if (error1) return console.error(error1);
-  fetchData(API + data1.results[0].id, function (error2, data2) {
-    if (error2) return console.error(error2);
-    fetchData(data2.origin.url, function (error3, data3) {
-      if (error3) return console.error(error3);
-      console.log(data1.info.count);
-      console.log(data2.name);
-      console.log(data3.dimension);
     });
+    xhttp.send();
   });
-});
+}
+
+
+fetchData(API)
+  .then(data => {
+    console.log(data.info.count);
+    return fetchData(`${API}${data.results[0].id}`)
+  })
+  .then(data => {
+    console.log(data.name);
+    return fetchData(data.origin.url)
+  })
+  .then(data => {
+    console.log(data.dimension);
+  })
+  .catch(err => console.error(err));
