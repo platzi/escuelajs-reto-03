f64f1b808462341e989b180e56093e70588a2446
Juan Daniel Rios Ramirez <55267847+Mrdaniel01@users.noreply.github.com>
2020-07-31 14:33:18 -0500

    [UPDATE] Challenge 3 - done

--- src/index.js
+++ src/index.js
@@ -1,32 +1,40 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 var API = 'https://rickandmortyapi.com/api/character/';
-var xhttp = new XMLHttpRequest();
 
-function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === '4') {
-      if (xhttp.status == 200)
-        callback(null, xhttp.responseText);
-      else return callback(url_api);
-    }
-  };
-  xhttp.open('GET', url_api, false);
-  xhttp.send();
-};
-
-fetchData(API, function (error1, data1) {
-  if (error1) return console.error('Error' + ' ' + error1);
-  console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
-    if (error2) return console.error(error1);
-    console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
-      if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
+const fetchData = (url_api) => {
+  return new Promise ((resolve, reject) =>{
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url_api, true);
+    xhttp.onreadystatechange = (() => {
+    if (xhttp.readyState === 4){
+      (xhttp.status === 200)
+        ? resolve(JSON.parse(xhttp.responseText))
+        : reject (new Error(`Error ${url_api}`))
+      }
     });
+    xhttp.send();
   });
-});
+}
+
+
+fetchData(API)
+  .then(data => {
+    console.log('Primer Llamado...') ;
+    console.log(data.info.count);
+    return fetchData(`${API} ${data.results[0].id}`);
+  })
+  .then(data =>{
+    console.log('Segundo Llamado...');
+    console.log(data.name);
+    return fetchData(data.origin.url);
+  })
+.then(data =>{
+  console.log('Tercero Llamado...');  
+  console.log(data.dimension);
+})
+.catch(err => console.error(err));
+
+
+
+
