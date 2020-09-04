0835561189aa4b3f52c77dc12c806ccca444a74c
acrux95 <67486473+ArmandoCloud@users.noreply.github.com>
2020-09-04 01:24:48 -0500

    solving the exercise3

--- src/index.js
+++ src/index.js
@@ -1,36 +1,39 @@
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const API = 'https://rickandmortyapi.com/api/character/';
 
 
-const fetchData = (url_api, callback) => {
-  const xhttp = new XMLHttpRequest();
-  xhttp.open('GET', url_api, true);
-  xhttp.onreadystatechange = (event) => {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status == 200) {
-        callback(null, JSON.parse(xhttp.responseText));
-      } else {
-        const error = new Error(`Error ${url_api}`);
-        return callback(error, null)
-      }
-    }
-  }
-  xhttp.send();
+const fetchData = (url_api) => {
+  return new Promise((resolve, reject) => {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url_api, true);
+    xhttp.onreadystatechange = (() => {
+      if (xhttp.readyState === 4) {
+           (xhttp.status == 200)
+            ? resolve (JSON.parse(xhttp.responseText))
+            : reject (new Error(`Error ${url_api}`));
+        }
+    })
+    xhttp.send();
+  })
 }
 
-fetchData(API, (error1, data1) => {
-  if (error1) return console.error(error1);
-    console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, (error2, data2) => {
-    if (error2) return console.error(error2);
-      console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, (error3, data3) => {
-      if (error3) return console.error(error3);
-      console.log(`Tercero Llamado...
-      
-Personajes: ${data1.info.count}
-Primer Personaje: ${data2.name}
-Dimensión: ${data3.dimension}`);
-    })
+fetchData(API)
+  .then(data => {
+    console.log(`
+    Primer Llamado...
+    Segundo Llamado...
+    Tercer Llamado...
+
+    Personajes: ${data.info.count}`);
+    return fetchData(`${API}${data.results[0].id}`);
   })
-})
+  .then(data => {
+    console.log(` 
+    Primer Personaje: ${data.name}`);
+    return fetchData(data.origin.url); 
+  })
+  .then(data => {
+    console.log(`
+    Dimensión: ${data.dimension}`);
+  })
+  .catch(err => console.error(err));
--- src/script/index.dev.js
+++ src/script/index.dev.js
@@ -4,33 +4,29 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 var API = 'https://rickandmortyapi.com/api/character/';
 
-var fetchData = function fetchData(url_api, callback) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.open('GET', url_api, true);
+var fetchData = function fetchData(url_api) {
+  return new Promise(function (resolve, reject) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url_api, true);
 
-  xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status == 200) {
-        callback(null, JSON.parse(xhttp.responseText));
-      } else {
-        var error = new Error("Error ".concat(url_api));
-        return callback(error, null);
+    xhttp.onreadystatechange = function () {
+      if (xhttp.readyState === 4) {
+        xhttp.status == 200 ? resolve(JSON.parse(xhttp.responseText)) : reject(new Error("Error ".concat(url_api)));
       }
-    }
-  };
+    };
 
-  xhttp.send();
+    xhttp.send();
+  });
 };
 
-fetchData(API, function (error1, data1) {
-  if (error1) return console.error(error1);
-  console.log('Primer Llamado...');
-  fetchData(API + data1.results[0].id, function (error2, data2) {
-    if (error2) return console.error(error2);
-    console.log('Segundo Llamado...');
-    fetchData(data2.origin.url, function (error3, data3) {
-      if (error3) return console.error(error3);
-      console.log("Tercero Llamado...\n      \nPersonajes: ".concat(data1.info.count, "\nPrimer Personaje: ").concat(data2.name, "\nDimensi\xF3n: ").concat(data3.dimension));
-    });
-  });
+fetchData(API).then(function (data) {
+  console.log("\n    Primer Llamado...\n    Segundo Llamado...\n    Tercer Llamado...\n\n    Personajes: ".concat(data.info.count));
+  return fetchData("".concat(API).concat(data.results[0].id));
+}).then(function (data) {
+  console.log(" \n    Primer Personaje: ".concat(data.name));
+  return fetchData(data.origin.url);
+}).then(function (data) {
+  console.log("\n    Dimensi\xF3n: ".concat(data.dimension));
+})["catch"](function (err) {
+  return console.error(err);
 });
