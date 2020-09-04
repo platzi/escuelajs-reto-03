78c89a4bfc9fc11ec78a9457a03b418c9b8caa77
acrux95 <67486473+ArmandoCloud@users.noreply.github.com>
2020-09-03 23:53:10 -0500

    solving the exercise2

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var API = 'https://rickandmortyapi.com/api/character/';
 
 
-function fetchData(url_api, callback) {
-  var xhttp = new XMLHttpRequest();
+const fetchData = (url_api, callback) => {
+  const xhttp = new XMLHttpRequest();
   xhttp.open('GET', url_api, true);
-  xhttp.onreadystatechange = function (event) {
+  xhttp.onreadystatechange = (event) => {
     if (xhttp.readyState === 4) {
       if (xhttp.status == 200) {
         callback(null, JSON.parse(xhttp.responseText));
       } else {
-        var error = new Error('Error', + url_api);
+        const error = new Error(`Error ${url_api}`);
         return callback(error, null)
       }
     }
@@ -18,18 +18,19 @@ function fetchData(url_api, callback) {
   xhttp.send();
 }
 
-fetchData(API, function (error1, data1) {
+fetchData(API, (error1, data1) => {
   if (error1) return console.error(error1);
     console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, function (error2, data2) {
+  fetchData(API + data1.results[0].id, (error2, data2) => {
     if (error2) return console.error(error2);
       console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, function (error3, data3) {
+    fetchData(data2.origin.url, (error3, data3) => {
       if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log('Personajes:' + ' ' + data1.info.count);
-      console.log('Primer Personaje:' + ' ' + data2.name);
-      console.log('Dimensión:' + ' ' + data3.dimension);
+      console.log(`Tercero Llamado...
+      
+Personajes: ${data1.info.count}
+Primer Personaje: ${data2.name}
+Dimensión: ${data3.dimension}`);
     })
   })
 })
