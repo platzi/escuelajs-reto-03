6ceee236b861a303c136aed94933f045db31deec
Sergio Hortua <sergioandresjsv@gmail.com>
2020-09-18 23:51:22 -0500

    Tercer punto resuelto

---
+++ src/utils/fetchData.js
@@ -0,0 +1,21 @@
+
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+
+const fetchData = (url_api) => {
+    return new Promise((resolve, reject) => {
+        const xhttp = new XMLHttpRequest();
+        xhttp.open('GET', url_api, true);
+        xhttp.onreadystatechange = (() => {
+            if (xhttp.readyState === 4) {
+                (xhttp.status === 200)
+                    ? resolve(JSON.parse(xhttp.responseText))
+                    : reject(new Error('Error' + url_api))
+            }
+        });
+        xhttp.send();
+    });
+
+};
+
+module.exports = fetchData;
