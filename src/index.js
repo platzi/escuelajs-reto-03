cf65a455e6427580ae573f4cbd0bb1b5ee14111c
andresmesad <andresf.mesad@gmail.com>
2020-10-17 21:26:11 -0500

    Third problem solved

--- src/index.js
+++ src/index.js
@@ -1,36 +1,38 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var API = 'https://rickandmortyapi.com/api/character/';
-var xhttp = new XMLHttpRequest();
+const API = 'https://rickandmortyapi.com/api/character/';
 
-function fetchData(url_api, callback) {
-  xhttp.onreadystatechange = () => {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status === 200)
-        callback(null, JSON.parse(xhttp.responseText));
-      else {
-        const error = new Error('Error en el llamado de la API')
-        return callback(error, null)
-      } 
-      
+
+const fetchData = (url_api) => {
+  return new Promise((resolve, reject) => {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url_api, false);
+    xhttp.onreadystatechange = () => {
+      if (xhttp.readyState === 4) {
+        if (xhttp.status === 200) {
+          resolve(JSON.parse(xhttp.responseText))
+        }
+      } else {
+        reject(new Error('Error al consultar la API'))
+      }
     }
-  };
-  xhttp.open('GET', url_api, false);
-  xhttp.send();
-};
+    xhttp.send()
+  })
+}
 
-fetchData(API, (error1, data1) =>  {
-  if (error1) return console.error('Error' + ' ' + error1);
-  console.log('Primer Llamado...')
-  fetchData(API + data1.results[0].id, (error2, data2) => {
-    if (error2) return console.error(error1);
+fetchData(API)
+  .then(data => {
+    console.log('Primer Llamado...')
+    console.log(`Personajes: ${data.info.count}`);
+    return fetchData(`${API}${data.results[0].id}`); 
+  })
+  .then(data =>{
     console.log('Segundo Llamado...')
-    fetchData(data2.origin.url, (error3, data3) => {
-      if (error3) return console.error(error3);
-      console.log('Tercero Llamado...')
-      console.log(`Personajes: ${data1.info.count}`);
-      console.log(`Primer Personaje: ${data2.name}`);
-      console.log(`Dimensión: ${data3.dimension}`);
-    });
-  });
-});
+    console.log(`Primer Personaje: ${data.name}`);
+    return fetchData(data.origin.url);
+  })
+  .then(data => {
+    console.log('Tercero Llamado...')
+    console.log(`Dimensión: ${data.dimension}`);
+  })
+  .catch(e => console.log(e))
